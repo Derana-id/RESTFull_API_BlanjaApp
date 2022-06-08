b6856c58180c7d27bc71d783d5252d7cb05197c5
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-08 14:05:10 +0700

    Base configuration

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/pg');
+
+const User = db.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    token: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    is_verified: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+  }
+);
+
+module.exports = User;
