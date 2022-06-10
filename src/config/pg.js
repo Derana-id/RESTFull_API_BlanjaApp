3bd6255c09b54de49bdd95dafe06ad6f6dd89327
altrawan <muhammadalifputrasetiawan@gmail.com>
2022-06-10 08:14:29 +0700

    Setup production mode

--- src/config/pg.js
+++ src/config/pg.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
-
 const {
+  NODE_ENV,
   DB_HOST,
   DB_USER,
   DB_PASSWORD,
@@ -10,11 +10,27 @@ const {
 } = require('../helpers/env');
 
 // create connection
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
-  port: DB_PORT,
-  dialect: DB_DIALECT,
-});
+let db = null;
+// Mode Development
+if (NODE_ENV === 'development') {
+  db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+  });
+}
+
+// Mode Production
+if (NODE_ENV === 'production') {
+  db = new Sequelize(DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+}
 
 // export connection
 module.exports = db;
